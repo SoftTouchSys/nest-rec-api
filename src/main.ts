05b0cf18f7f4fa644e1614699f79d2c380c92a2e
SoftTouchSys <72387777+SoftTouchSys@users.noreply.github.com>
2022-09-23 19:30:29 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   //   origin:[`${env.FRONT_END_URL}`]
   // });
   app.enableCors() 
-  var port = env.END_POINT_URL || 8080
-  await app.listen(port);
+  // var port = env.END_POINT_URL || 3000
+  await app.listen(env.END_POINT_URL || 3000);
 }
 bootstrap();
