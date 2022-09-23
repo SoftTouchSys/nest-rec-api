8d9c56adf9db7680d5e1472b6be5781e8dfb22a9
SoftTouchSys <72387777+SoftTouchSys@users.noreply.github.com>
2022-09-24 00:42:17 +0500

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Server Is Up and runing!';
+    return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   //   origin:[`${env.FRONT_END_URL}`]
   // });
   app.enableCors() 
-  await app.listen(env.END_POINT_URL || 3012);
+  await app.listen(env.END_POINT_URL || 3000);
 }
 bootstrap();
